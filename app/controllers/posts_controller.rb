7e1585eeb45265764c694fdde19966a71b6b9e26
lily-coder <lilyondie@gmail.com>
2022-03-26 23:52:53 +0300

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,5 @@
-# rubocop:disable Style/Documentation
 class PostsController < ApplicationController
   def index; end
 
   def show; end
 end
-# rubocop:enable Style/Documentation
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,2 @@
-# rubocop:disable Style/Documentation
 module ApplicationHelper
 end
-# rubocop:enable Style/Documentation
