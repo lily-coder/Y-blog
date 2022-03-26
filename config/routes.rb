3af89a3db944e8add20907bad4807bbe1fc8fb71
lily-coder <lilyondie@gmail.com>
2022-03-26 23:17:14 +0300

    Fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get "/posts/", to: "posts#index"
-  get "/posts/", to: "posts#show"
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
+  get '/posts/', to: 'posts#index'
+  get '/posts/', to: 'posts#show'
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
   end
   # Defines the root path route ("/")
   # root "articles#index"
