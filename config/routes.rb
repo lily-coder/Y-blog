c1e175a71fae295c0a6705bcd709afd0e7ca0cb1
lily-coder <lilyondie@gmail.com>
2022-03-27 00:37:14 +0300

    Get rid of unnecessary code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get '/posts/', to: 'posts#index'
-  get '/posts/', to: 'posts#show'
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show]
