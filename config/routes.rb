70655108128ffc6c1c4f23d0f564e8aca09b9993
Lilian Moraa <lilyondie@gmail.com>
2022-04-10 00:53:38 +0300

    Revert "Blog App: validations, model specs, and n+1 problems"

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
-  end
-  resources :posts do
-    resources :comments, only: %i[create]
-    resources :likes, only: %i[create]
+  resources :posts, only: %i[index show]
   end
 end
