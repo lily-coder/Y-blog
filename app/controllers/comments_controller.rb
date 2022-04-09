051c747b8d1f1db6d7d46e7c7dd9ccc6a88d7982
lily-coder <lilyondie@gmail.com>
2022-04-10 00:13:28 +0300

    Fix linter erors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,10 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
   def create
-
     @comment = Comment.new(comment_params)
     @comment.author_id = current_user.id
     @comment.post_id = params[:post_id]
+
     if @comment.save
       flash[:notice] = 'Comment created succesfully'
       redirect_to user_posts_path(current_user)
