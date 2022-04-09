0504f3f8319d9a9ec8648da0de54c06eaec473f9
lily-coder <lilyondie@gmail.com>
2022-04-09 13:09:08 +0300

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :comments
-    has_many :likes
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
 
-    def update_post_counter
-        author.increment!(:posts_counter)
-    end
+  def update_post_counter
+      author.increment!(:posts_counter)
+  end
 
-    def recent_comments
-        comments.order('created_at Desc').limit(5)
-    end      
+  def recent_comments
+      comments.order('created_at Desc').limit(5)
+  end      
 end
