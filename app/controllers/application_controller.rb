1a65ebd7264f3448ab4a7628fb62946da7efa674
lily-coder <lilyondie@gmail.com>
2022-04-16 16:17:19 +0300

    Add authentication with cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio])
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :avatar, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index new create show destroy]
   end
   resources :posts do
-    resources :comments, only: %i[create]
+    resources :comments, only: %i[create destroy]
     resources :likes, only: %i[create]
   end
+  resources :comments, only: %i[destroy]
+  resources :likes, only: %i[destroy]
 end
